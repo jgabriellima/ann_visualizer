c20294de61ac041a3ff59d1642745b1907f07e35
João Gabriel Lima <joaogabriellima.eng@gmail.com>
2018-05-06 19:00:16 -0300

    script for model testing

---
+++ test.py
@@ -0,0 +1,27 @@
+# Create your first MLP in Keras
+from keras.models import model_from_json
+import numpy
+
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+# load pima indians dataset
+dataset = numpy.loadtxt("pima-indians-diabetes.data.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+X = dataset[:,0:8]
+Y = dataset[:,8]
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+
+# load weights into new model
+loaded_model.load_weights("model.h5")
+print("Loaded model from disk")
+
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+score = loaded_model.evaluate(X, Y, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1] * 100))
